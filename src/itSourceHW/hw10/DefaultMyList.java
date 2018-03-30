c08c5e1e62e2a9a1a67c4bc2c3e81d23b0233a22
Andrii Sviatobatko <svyatobatko.a@gmail.com>
2018-03-31 00:05:45 +0300

    first commit

---
+++ src/itSourceHW/hw10/DefaultMyList.java
@@ -0,0 +1,52 @@
+package itSourceHW.hw10;
+
+public class DefaultMyList<e> implements MyList {
+
+	@Override
+	public void add(Object e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean remove(Object o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object[] toArray() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String toString() {
+		return "SomeString";
+	}
+	
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean contains(Object o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsAll(MyList c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ src/itSourceHW/hw10/HomeWork10.java
@@ -0,0 +1,10 @@
+package itSourceHW.hw10;
+
+import java.util.List;
+
+public class HomeWork10 {
+	public static void main(String[] args) {
+		List<String> list = new DefaultMyList<>();
+	}
+}
+
